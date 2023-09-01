2dde0bedf385272f3bbaeed60f63a59c61d6e257
Favour Nwevo <favour@rxhealthinfosystems.com>
2023-09-01 11:15:19 +0000

    modified the AppServiceProvider File

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Exception;
 use Illuminate\Support\Facades\DB;
 use Database\Seeders\SettingSeeder;
 use Illuminate\Support\Facades\Schema;
@@ -21,12 +22,24 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
+    // check if database exist
     protected function databaseExists($databaseName)
     {
         $query = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$databaseName'";
         $result = DB::select($query);
 
         return count($result) > 0;
+    }
+
+    // check if database connection exist
+    protected function databaseConnectionExists()
+    {
+        try {
+            DB::connection()->getPdo();
+            return true;
+        } catch (Exception) {
+            return false;
+        }
     }
 
     /**
@@ -36,30 +49,38 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // set the default string length
         Schema::defaultStringLength(191);
-        $setting = \App\Models\Setting::first() ?? null;
 
-        // check if database exist, if not exist then create the database
-        $databaseName = config('database.connections.mysql.database');
+        // check if there is database connection
+        if ($this->databaseConnectionExists()) {
+            // check if database exist, if not exist then create the database
+            $databaseName = config('database.connections.mysql.database');
+            if (!$this->databaseExists($databaseName)) {
+                $charset = config('database.connections.mysql.charset', 'utf8mb4');
+                $collation = config('database.connections.mysql.collation', 'utf8mb4_unicode_ci');
 
-        if (!$this->databaseExists($databaseName)) {
-            $charset = config('database.connections.mysql.charset', 'utf8mb4');
-            $collation = config('database.connections.mysql.collation', 'utf8mb4_unicode_ci');
+                DB::statement("CREATE DATABASE $databaseName CHARACTER SET $charset COLLATE $collation");
+            }
 
-            DB::statement("CREATE DATABASE $databaseName CHARACTER SET $charset COLLATE $collation");
+
+            // check if setting is not null, if not null then run the SettingSeeder to create the setting
+            $setting = \App\Models\Setting::first() ?? null;
+            if ($setting == null) {
+                // run the migration
+                Artisan::call('migrate');
+                // run the seeder
+                Artisan::call('db:seed', [
+                    '--class' => SettingSeeder::class
+                ]);
+            }
+            // share the setting variable to all the views
+            view()->share('setting', $setting);
         }
+        // else {
+        //     throw new \Exception('No database connection found. Please run your database server or check your database connection configuration in .env file.');
+        // }
 
-        // check if setting is not null, if not null then run the SettingSeeder to create the setting
-        if ($setting == null) {
-            // run the migration
-            Artisan::call('migrate');
-            // run the seeder
-            Artisan::call('db:seed', [
-                '--class' => SettingSeeder::class
-            ]);
-        }
-        // share the setting variable to all the views
-        view()->share('setting', $setting);
 
         // create a macro search for data range in the builder
         Builder::macro('searchDateRange', function ($from, $to) {
